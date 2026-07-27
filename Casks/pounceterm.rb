3b8aba520c236deeea464081a43d252a8af6da40
Steve Sparks <15949542+StevenSSparks@users.noreply.github.com>
2026-07-26 19:11:29 -0500

    pounceterm: update to 0.3.1

--- Casks/pounceterm.rb
+++ Casks/pounceterm.rb
@@ -1,6 +1,6 @@
 cask "pounceterm" do
-  version "0.3.0"
-  sha256 "589ecc976b7cf77f7a3a42d16a92685f03f7932cae238bc82ae0992b9e9541f2"
+  version "0.3.1"
+  sha256 "206a2432f820a7651ecac7b25f07e6c437396a54956f223f4ebd293370becc8c"
 
   url "https://github.com/pounceapps/downloads/releases/download/pounceterm-v#{version}/PounceTERM-#{version}.dmg"
   name "PounceTERM"
